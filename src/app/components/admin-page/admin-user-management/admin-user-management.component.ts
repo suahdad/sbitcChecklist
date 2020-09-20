fc0d7ce39bc63b105ad63511a5373720330b8c3b
jpasol <38642594+jpasol@users.noreply.github.com>
2020-09-20 20:31:14 +0800

    Revert "Revert "Updated User Mgmt Table""
    This reverts commit f73ca6535513582e63f45cc7badd5edd6b9e6cb9.

--- src/app/components/admin-page/admin-user-management/admin-user-management.component.ts
+++ src/app/components/admin-page/admin-user-management/admin-user-management.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin-user-management',
   templateUrl: './admin-user-management.component.html',
-  styleUrls: ['./admin-user-management.component.css']
+  styleUrls: ['./admin-user-management.component.scss']
 })
-export class AdminUserManagementComponent implements OnInit {
+export class AdminUserManagementComponent implements AfterViewInit {
 
-  constructor() { }
+  users: User[] = new Array<User>();
+  displayedColumns: string[] = ['id', 'lastName' ,'firstName', 'middleName', 'isActive']
+  dataSource = new MatTableDataSource(this.users);
 
-  ngOnInit() {
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(private userService: UserService) { }
+
+  ngAfterViewInit() {
+    this.refreshData()
+    
+  }
+
+  refreshData(){
+    this.userService.getUsers().subscribe( data => {
+      this.users = data
+
+
+      this.dataSource.data = this.users;
+
+    })
   }
 
 }
