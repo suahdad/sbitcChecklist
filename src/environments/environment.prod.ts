4ed5ac4f0ed2d6b97e430ccee7e6e8c21ca7730f
jpasol <38642594+jpasol@users.noreply.github.com>
2020-10-20 08:23:15 +0800

    modified environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://192.168.11.243:8080'
+  apiURL: 'http://10.88.140.68:10080',
+  ecN4Url: 'http://10.122.8.114:8280/ecn4web/servlet/xmlrdt'
   // apiURL: 'https://10.88.140.133:44350'
 };
