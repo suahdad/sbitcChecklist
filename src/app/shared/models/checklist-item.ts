4293a254ef1d30f96bdc90d3d7e5c51bf6d58d36
jpasol <38642594+jpasol@users.noreply.github.com>
2020-11-11 18:02:01 +0800

    added parent property to some classes

--- src/app/shared/models/checklist-item.ts
+++ src/app/shared/models/checklist-item.ts
@@ -1,8 +1,11 @@
+import { Component } from '@angular/core';
 import { Checklist } from './checklist';
 
 export class ChecklistItem {
+    checklist? : Checklist
     checklistid?: number;
     equipment_typeid: string;
+    component?: Component
     componentid: string;
     conditionid: string; 
     remarks: string;
