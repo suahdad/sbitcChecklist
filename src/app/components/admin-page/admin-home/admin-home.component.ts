57ceb46bc31c7ff1acd915ca2d5f52438be1eef3
jpasol <38642594+jpasol@users.noreply.github.com>
2020-12-02 13:55:06 +0800

    added initla bar chart

--- src/app/components/admin-page/admin-home/admin-home.component.ts
+++ src/app/components/admin-page/admin-home/admin-home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { ChecklistService } from 'src/app/services/checklist.service';
+import { FormatterService } from 'src/app/services/formatter.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -8,9 +12,69 @@ import { Router } from '@angular/router';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public isChartReady;
+  public chartType: ChartType = "bar"
+  public chartLegends = true
+  public chartDataSet = []
+  public chartLabels: Label[] = []
+  public chartPlugins = []
+  public chartOptions: ChartOptions = {
+    responsive: true
+  }
+
+  private components = ['AIRLK',
+    'ATTCH',
+    'CLEAN',
+    'COLNT',
+    'DSPLY',
+    'ENGIN',
+    'FREXT',
+    'HYOIL',
+    'LIGHT',
+    'OILLK',
+    'SDMRR',
+    'SPRDR',
+    'TIRES',
+    'TWIST',
+    'WIPER',
+    'WSHLD']
+
+  private cmpDescription = ['Air Leaks',
+    'Attachment Functions',
+    'Cleanliness',
+    'Coolant',
+    'Display',
+    'Engine',
+    'Fire Extinguisher',
+    'Hydraulic Oil',
+    'Lights',
+    'Oil Leaks',
+    'Side Mirror',
+    'Spreader',
+    'Tyres',
+    'Twist Lock',
+    'Wiper',
+    'Windshield']
+
+  
+
+  constructor(private router: Router,
+    private chkService: ChecklistService,
+    private frmService: FormatterService) {
+      this.chkService.getChecklistItemsWithIssues().subscribe(x => {
+        this.chartLabels = this.cmpDescription
+        this.chartDataSet = this.frmService.getCountByKey("componentID",
+        this.components,
+        "conditionID",
+        "OTHER",
+        x)
+        this.isChartReady = true;
+      })
+
+     }
 
   ngOnInit() {
+
   }
 
   alertUnavailable(){
