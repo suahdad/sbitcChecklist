1ecd43693bfe3c6e7e5f18f4415aa5e6d24395c1
jpasol <38642594+jpasol@users.noreply.github.com>
2021-01-13 22:45:53 +0800

    added unit tests

--- src/app/components/login-equipment/equipment.component.spec.ts
+++ src/app/components/login-equipment/equipment.component.spec.ts
@@ -1,4 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NativeDateAdapter } from '@angular/material/core';
+import { By, disableDebugTools } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { by } from 'protractor';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { EquipmentComponent } from './equipment.component';
 
@@ -8,7 +17,12 @@ describe('EquipmentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EquipmentComponent ]
+      declarations: [ EquipmentComponent ],
+      imports: [ReactiveFormsModule,
+      HttpClientModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      FontAwesomeModule]
     })
     .compileComponents();
   }));
@@ -21,5 +35,60 @@ describe('EquipmentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should say Equipment for form-header', () => {
+    var h3 = fixture.debugElement.query(By.css('#form-header')).nativeElement;
+    expect(h3.innerText).toEqual('Equipment')
+  });
+  it('should have icon for input equip-icon', () => {
+    var inputIcon = fixture.debugElement.query(By.css('#equip-icon')).childNodes;
+    var tagList = inputIcon.map(x => x.nativeNode['tagName']);
+    expect(tagList).toContain('svg');
+  });
+  it('should contain \'Ex.\' placeholder for input-equip', () => {
+    var inputEquip = fixture.debugElement.query(By.css('#input-equip')).nativeElement;
+    expect(inputEquip.placeholder).toContain('Ex.');
+  });
+  it('should say \'Proceed\' in form-button', () => {
+    var button = fixture.debugElement.query(By.css('#form-button')).nativeElement;
+    expect(button.value).toContain('Proceed');
+  });
+  it('should contain \'Forget\' in helper-text', () => {
+    var helper = fixture.debugElement.query(By.css('#helper-text')).nativeElement;
+    expect(helper.innerText).toContain('Forget');
+  });
+  it('should have logout link', () => {
+    var logout = fixture.debugElement.query(By.css('#logout-link')).nativeElement;
+    expect(logout).toBeTruthy();
+  }) 
+  // it('should logout when logout is pressed', () => {
+  //   var logout = fixture.debugElement.query(By.css('#logout-link')).nativeElement;
+  //   logout.click();
+  //   //cannot be done as there is no login test
+  // }) 
+  it('should initialize isSubmitted to false', () => {
+    expect(component.isSubmitted).toBeFalsy();
+  }); 
+  it('should initialize invalidEquip to false', () => {
+    expect(component.invalidEquip).toBeFalsy();
+  });
+  // it('should show error message on error submit', () => {
+  //   component.invalidEquip = true;
+  //   fixture.detectChanges();
+  //   var message = fixture.debugElement.query(By.css('#error-message'))
+  //   console.log(component)
+  //   expect(message).toBeTruthy();
+  // });
+  it('should disable button on submit', () => {
+    const button = fixture.debugElement.query(By.css('#form-button'));
+    button.nativeElement.click();
+    fixture.detectChanges();
+    
+    let targetObj = new Proxy(button.nativeElement, {
+      set: (target, key, value) => {
+        console.log({key,value})
+        return true
+      }
+    })
   });
 });
