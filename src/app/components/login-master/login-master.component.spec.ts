2e78330d07c220d871778f53b053fc2c73027e14
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-08 08:37:06 +0800

    added unit tests
    - applied id for login components

--- src/app/components/login-master/login-master.component.spec.ts
+++ src/app/components/login-master/login-master.component.spec.ts
@@ -1,5 +1,14 @@
+import { FixedSizeVirtualScrollStrategy } from '@angular/cdk/scrolling';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { EquipmentComponent } from '../login-equipment/equipment.component';
+import { LoginComponent } from '../login/login.component';
 import { LoginMasterComponent } from './login-master.component';
 
 describe('LoginMasterComponent', () => {
@@ -8,7 +17,14 @@ describe('LoginMasterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginMasterComponent ]
+      declarations: [ LoginMasterComponent ,
+        EquipmentComponent ,
+        LoginComponent ],
+      imports: [ HttpClientModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        FontAwesomeModule,
+        BrowserAnimationsModule]
     })
     .compileComponents();
   }));
@@ -21,5 +37,45 @@ describe('LoginMasterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  // it('should have background', () => {
+  //   console.log(fixture.debugElement.query(By.css('body')))
+  //   expect(component).toBeTruthy();
+  // }); //untestable, cannot find css styles.
+  it('should initially show login-component only', () => {
+    let loginComp = fixture.debugElement.query(By.css('#login-component'))
+    expect(loginComp).toBeTruthy();
+  });
+  it('should initially have equip-component unloaded', () => {
+    let equipComp = fixture.debugElement.query(By.css('#equip-component'))
+    expect(equipComp).toBeFalsy();
+  });
+  it('should hide login-component after successful login', () => {
+    let loginComp = fixture.debugElement.query(By.css('#login-component'))
+    let loginInstance : LoginComponent = loginComp.componentInstance;
+    spyOn(loginInstance,'onSubmit').and.callFake( () => {
+      component.user = true;
+    })
+
+    let loginBtn = loginComp.query(By.css('#form-login')).nativeElement;
+    loginBtn.click();
+    fixture.detectChanges();
+
+    let newLoginComp = fixture.debugElement.query(By.css('#login-component')); //added to check again
+    expect(newLoginComp).toBeFalsy();
+  });
+  it('should show equip-component after successful login', () => {
+    let loginComp = fixture.debugElement.query(By.css('#login-component'))
+    let loginInstance : LoginComponent = loginComp.componentInstance;
+    spyOn(loginInstance,'onSubmit').and.callFake( () => {
+      component.user = true;
+    })
+
+    let loginBtn = loginComp.query(By.css('#form-login')).nativeElement;
+    loginBtn.click();
+    fixture.detectChanges();
+
+    let equipComp = fixture.debugElement.query(By.css('#equip-component'))
+    expect(equipComp).toBeTruthy();
   });
 });
