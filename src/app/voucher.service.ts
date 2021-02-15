fa56e8ab8e45062da3c284c9e7d0d01da5545f72
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-15 14:13:06 +0800

    added get post for voucher

---
+++ src/app/voucher.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
+import { environment } from 'src/environments/environment';
+import { Equipment } from './shared/models/equipment';
+import { User } from './shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoucherService {
+
+  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.apiURL}/api/vouchers`
+
+
+  public getVoucher(id: string){
+    return this.http.get<any>(`${this.apiUrl}/${id}`)
+  }
+
+  public getVouchers(){
+    return this.http.get<any>(`${this.apiUrl}`)
+  }
+
+  public postVoucher(userid, equipid){
+    var _voucher = {
+      userid: userid,
+      equipmentid : equipid
+    }
+
+    //added headers
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.set('Content-Type', 'application/x-www-form-urlencoded');
+
+
+    try{
+      return this.http.post<any>(`${this.apiUrl}`, _voucher, {headers: headers});
+    }catch(error){
+      console.log(error);
+      return error;
+    }
+  }
+}
