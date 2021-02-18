89f0d2ee3f1a83552fd64641944b326b64a779ae
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-18 16:13:26 +0800

    vocucher save function

--- src/app/components/login-master/login-master.component.ts
+++ src/app/components/login-master/login-master.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authentication/auth.service';
-// import { AuthService } from '../../services/mock/fake-authentication.service';
 import { slider } from '../../route-animations'
+import { VoucherService } from 'src/app/voucher.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-master',
@@ -14,13 +15,25 @@ export class LoginMasterComponent implements OnInit {
   user: any;
   equip: any;
 
-  constructor(private auth:AuthService) { 
+  constructor(private auth:AuthService,
+    private voucherService: VoucherService,
+    private router: Router) { 
     this.auth.currentUser.subscribe(data => this.user = data)
     this.auth.currentEquipment.subscribe(data => this.equip = data)
   }
 
   ngOnInit() {
 
+  }
+
+  voucherSave(){
+    if(this.user && this.equip){
+      this.voucherService.postVoucher(this.user.id,this.equip.id)
+
+      this.router.navigate(['']);
+      //added because sometimes after submission it doesn't go into the main form
+      location.reload();
+    }
   }
 
 }
