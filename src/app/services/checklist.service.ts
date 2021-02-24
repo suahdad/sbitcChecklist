cd9e2d5d26ecd292ebea1775d1238b9f3aab497e
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-24 14:24:21 +0800

    removed unused

--- src/app/services/checklist.service.ts
+++ src/app/services/checklist.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from './authentication/auth.service';
-import { stringToKeyValue } from '@angular/flex-layout/extended/typings/style/style-transforms';
 import { environment } from 'src/environments/environment';
 import { Checklist } from '../shared/models/checklist';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ChecklistItem } from '../shared/models/checklist-item';
 
@@ -24,9 +20,7 @@ export class ChecklistService {
   public isSubmitSuccess: boolean;
 
   constructor(private http: HttpClient,
-    private authService: AuthService,
-    private fb: FormBuilder,
-    private router: Router) { }
+    private fb: FormBuilder) { }
 
   public submitChecklist(data: Checklist) {
      return this.http.post<Checklist>(this.apiUrl,data);
