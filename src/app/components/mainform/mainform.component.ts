11910d02a3f7bdf77b58d78b603f3bf37e9cad5f
jpasol <38642594+jpasol@users.noreply.github.com>
2021-03-24 14:33:33 +0800

    optimization
    -moved value getters from constructor to ngOnInit
    - used value instead of subscriber to avoid memory leaks

--- src/app/components/mainform/mainform.component.ts
+++ src/app/components/mainform/mainform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authentication/auth.service'
 import { User } from 'src/app/shared/models/user';
+import { take } from 'rxjs/operators';
 // import { AuthService } from '../../services/mock/fake-authentication.service'
 
 @Component({
@@ -13,11 +14,15 @@ export class MainformComponent implements OnInit {
   eqType: any;
   user: User;
 
-  constructor(auth : AuthService) { 
-    auth.currentEquipment.subscribe( data => this.eqType = data.equipment_TypeID)
+  constructor(private auth : AuthService) { 
+    // auth.currentEquipment.subscribe( data => this.eqType = data.equipment_TypeID) //better to start at ngOninit 
+    //so there will be no more 'work' done on instantiation 
   }
 
   ngOnInit() {
+    // this.auth.currentEquipment.pipe(take(1)).subscribe(x => this.eqType = x.equipment_TypeID)
+    this.user = this.auth.currentUserValue
+    this.eqType = this.auth.currentEquipmentValue.equipment_TypeID;
   }
 
 }
